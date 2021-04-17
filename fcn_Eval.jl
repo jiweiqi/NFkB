8a394dfe62afb9b016b6981ee96c094ae1e9826a
jiweiqi <jiweiqi10@gmail.com>
2021-04-17 17:52:36 -0400

    try AD

--- fcn_Eval.jl
+++ fcn_Eval.jl
@@ -2,6 +2,12 @@ using Plots
 using DifferentialEquations
 using DelimitedFiles
 using MAT
+using ForwardDiff
+using Zygote
+using Flux
+using ProgressBars, Printf
+using Flux.Optimise:update!
+using Flux.Losses:mae, mse
 
 exp_data = matread("./orginal_model/exp_data.mat");
 TNF_exp = exp_data["TNF_exp"];
@@ -13,12 +19,39 @@ IkB_GO_e = exp_data["IkB_GO_e"];
 INIT = exp_data["INIT"];
 
 include("getRateParams.jl")
-include("nfkbOde.jl")
+include("nfkbOde_clean.jl")
 
 ## LPS injection without BFA
 v.flag_noTnfFeedback = false; # indicator that BFA is not added
 dose = vec([ 0.001 0.005 0.025 ] .* v.TP[13]); # four different input conditions
 t_sam = vec(Float64.([0 10 20 30 60 120 240 360])); # time instants when the measurements are taken
+
+## Test AD
+i = 1;
+INIT[18] = dose[i]
+u0 = vec(INIT);
+delta = similar(u0);
+t = 0.01;
+nfkbOde!(delta, u0, v, t)
+gu = ForwardDiff.jacobian((du, u) -> nfkbOde!(du, u, v, t), delta, u0)
+
+n_nodes = 2;
+ns = length(u0);
+nn = Chain(x -> x,
+        Dense(ns, n_nodes, gelu),
+        Dense(n_nodes, n_nodes, gelu),
+        Dense(n_nodes, n_nodes, gelu),
+        Dense(n_nodes, n_nodes, gelu),
+        Dense(n_nodes, ns))
+p, re = Flux.destructure(nn);
+
+function hybrid!(delta, u, p, t)
+    nfkbOde!(delta, u, v, t);
+    du = re(p)(u)
+    delta .+= du
+end
+hybrid!(delta, u0, p, t)
+
 
 titstr = ["10, ng/mL", "50, ng/mL", "250, ng/mL"];
 
@@ -26,7 +59,7 @@ for i = 1:length(dose)
     INIT[18] = dose[i]
 
     u0 = vec(INIT);
-    prob = ODEProblem(nfkbOde!, u0, (0.0, t_sam[end]), v);
+    prob = ODEProblem(hybrid!, u0, (0.0, t_sam[end]), p);
     sol = solve(prob, atol=1.e-6, rtol=1.e-3);
     IkBat = sum(sol'[:,1:4], dims=2)
     
